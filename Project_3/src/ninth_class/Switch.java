aec3aaf984dc726df36338f47b63fccaa089d67b
jitender.ahuja <jitender.ahuja@Jitender.idc.mihindia.com>
2018-10-14 14:37:29 +0530

    Class_9 | 2_Sep

---
+++ Project_3/src/ninth_class/Switch.java
@@ -0,0 +1,43 @@
+package ninth_class;
+
+public class Switch {
+
+public static void main(String[] args) {
+		
+		int a = 10;
+		int b = 10;
+		int z;
+		
+		char c = '*';
+		
+		
+		switch (c) {
+		case '+':
+			z=a+b;
+			System.out.println("Addition is: "+z);
+			break;
+
+		case '-':
+			z=a-b;
+			System.out.println("Subtraction is: "+z);
+			break;
+			
+		case '*':
+			z=a*b;
+			System.out.println("Multiplication is: "+z);
+			break;
+			
+		case '/':
+			z=a/b;
+			System.out.println("Division is: "+z);
+			break;
+			
+		default:
+			System.out.println("Invalid Opeartor");
+			break;
+		}	
+		
+	}
+
+	
+}
